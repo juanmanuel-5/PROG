99b44533268d45b7ed3e7e3d0f1b93b3953d3eef
juanmanuel-5 <kitsault@net-c.ca>
2023-10-06 15:24:06 +0200

    Ejercicios iniciales de Java

---
+++ Colores/src/App.java
@@ -0,0 +1,47 @@
+// import java.util.Calendar;
+
+public class App {
+
+  public static void main(String[] args) throws Exception {
+    System.out.print("\033[32m hierba");
+    System.out.print("\033[31m tomate");
+    System.out.print("\033[37m sábanas");
+    System.out.print("\033[36m cielo");
+    System.out.print("\033[35m nazareno");
+    System.out.print("\033[34m mar");
+    System.out.println(ConsoleColors.RESET);
+
+    System.out.println("┌────────┬───────────┬────────┬────────────────┐");
+    System.out.println("│ Código │   Color   │ Código │     Color      │");
+    System.out.println("├────────┼───────────┼────────┼────────────────┤");
+    System.out.print("│   30   │\033[30m  negro   \033[39;49m │");
+    System.out.println("   90   │\033[90m  negro claro  \033[39;49m │");
+    System.out.print("│   31   │\033[31m  rojo    \033[39;49m │");
+    System.out.println("   91   │\033[91m  rojo claro   \033[39;49m │");
+    System.out.print("│   32   │\033[32m  verde   \033[39;49m │");
+    System.out.println("   92   │\033[92m  verde claro  \033[39;49m │");
+    System.out.print("│   33   │\033[33m  amarillo\033[39;49m │");
+    System.out.println("   93   │\033[93m  amarillo claro\033[39;49m│");
+    System.out.print("│   34   │\033[34m  azul    \033[39;49m │");
+    System.out.println("   94   │\033[94m  azul claro   \033[39;49m │");
+    System.out.print("│   35   │\033[35m  morado  \033[39;49m │");
+    System.out.println("   95   │\033[95m  morado claro \033[39;49m │");
+    System.out.print("│   36   │\033[36m  cian    \033[39;49m │");
+    System.out.println("   96   │\033[96m  cian claro   \033[39;49m │");
+    System.out.print("│   37   │\033[37m  blanco  \033[39;49m │");
+    System.out.println("   97   │\033[97m  blanco claro \033[39;49m │");
+    System.out.println("└────────┴───────────┴────────┴────────────────┘");
+
+    // System.out.println("\033[31mHello, World!");
+
+    System.out.println(ConsoleColors.PURPLE + "Writing in purple color");
+    System.out.println(ConsoleColors.GREEN_BOLD + "Writing in green bold color");
+
+
+    // System.out.format(
+    //   ConsoleColors.YELLOW_BOLD + "Local Time: %tT",
+    //   Calendar.getInstance()
+    // );
+    System.out.print(ConsoleColors.RESET);
+  }
+}
