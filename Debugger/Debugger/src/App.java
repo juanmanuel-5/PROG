bb6a64b49127864ac9574b5642ae8ce66bb65121
juanmanuel-5 <kitsault@net-c.ca>
2023-10-31 21:17:23 +0100

    31/10/23 changes

---
+++ Debugger/Debugger/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a = 5;
+        int b = 16;
+        int suma = a + b;
+        System.out.printf("Addition result: %d", suma);
+    }
+}
