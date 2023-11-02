dc93bc56aedc1bab3fadb33749db0935c2d258a0
juanmanuel-5 <kitsault@net-c.ca>
2023-11-02 19:14:22 +0100

    02/11/23 changes

--- Debugger/Debugger/src/EuroSymbol.java
+++ Debugger/Debugger/src/EuroSymbol.java
@@ -1,8 +1,9 @@
-public class App {
+public class EuroSymbol {
     public static void main(String[] args) throws Exception {
         int a = 5;
         int b = 16;
         int suma = a + b;
-        System.out.printf("Addition result: %d", suma);
+        System.out.printf("Addition result: %d%s$€",suma, "\u20AC");
     }
 }
+// poner en consola >chcp 1252
